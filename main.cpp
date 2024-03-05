4d97f85e6f7f5f8c2a4f20a09e13e48e3aedebd9
rrs-sistema <rivaldo.roberto@outlook.com>
2024-03-04 23:13:58 -0300

    Iniciando o jogo

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>  // Essa bibliotca é usada quando queremos adicionar outras funções predefinidas da biblioteca de funções do pré-processado
+#include <stdlib.h> //Essa bibliotca possui funções envolvendo alocação de memória, controle de processos, conversões e outras...
+#include <conio.h>  // Essa  biblioteca(arquivo cabeçalho) de C usado principalmente por compiladores MS-DOS para fornecer input / output console
+
+
+char xadrez[8][8] = { // Cria o tabuleiro
+    {
+        'T', 'C', 'B', 'Q', 'K', 'B', 'C', 'T'
+    },
+    {
+        'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'
+    },
+    {
+        ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
+    },
+    {
+        ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
+    },
+    {
+        ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
+    },
+    {
+        ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
+    },
+    {
+        'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'
+    },{
+        't', 'c', 'b', 'q', 'k', 'b', 'c', 't'
+    },
+};
+
+int pintarTelaPreto(){
+    system("CLS");
+    int lin, col;
+    for (lin = 0; lin < 8; lin++) { //
+        for (col = 0; col < 8; col++) { //
+            if((lin + col) % 2 == 0) {
+                printf("\xB2");// Preenche a casinha
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    pintarTelaPreto();
+    getch(); // Espera o usuário interagir com o xadrez
+    return 0;
+}
+
+
